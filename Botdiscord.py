527a99176d810530ff5db807260cea7efc41b4eb
Hilokerx2 <96974758+Hilokerx2@users.noreply.github.com>
2025-02-25 17:53:20 +0700

    Add files via upload

---
+++ Botdiscord.py
@@ -0,0 +1,35 @@
+import discord
+from discord.ext import commands
+
+# กำหนดชื่อที่อนุญาต
+allowed_names = ["Shiro", "ชื่อ2", "ชื่อ3"]
+user_names = {}  # เก็บชื่อผู้ใช้และชื่อที่เลือก
+
+# สร้างบอท
+bot = commands.Bot(command_prefix="!")
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+
+@bot.command()
+async def Verifys(ctx, *, name: str):
+    user_id = ctx.author.id  # รหัสผู้ใช้ Discord ของคนที่เรียกใช้คำสั่ง
+
+    if name in allowed_names:
+        if name in user_names.values():
+            await ctx.send("ชื่อนี้ถูกใช้ไปแล้ว กรุณาเลือกชื่ออื่น.")
+        else:
+            user_names[user_id] = name
+            await ctx.send(f"คุณได้ตั้งชื่อเป็น: {name}")
+    else:
+        await ctx.send("ชื่อที่คุณใส่ไม่ถูกต้อง กรุณาใช้ชื่อที่กำหนดไว้")
+
+@bot.command()
+async def Check(ctx):
+    user_id = ctx.author.id
+    name = user_names.get(user_id, "คุณยังไม่ได้ตั้งชื่อ")
+    await ctx.send(f"ชื่อของคุณคือ: {name}")
+
+# ใส่ Token ของบอทที่นี่
+bot.run('MTM0Mzg4ODA3NDE3Nzc3MzU2OA.GPUNdv.WNiFP0V1YrzWy7BU-BoYN8o-1V95Su19Sxkzr0')
